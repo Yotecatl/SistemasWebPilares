af758dcd55dbdd66abb1cc640ef65f154ab80a20
Yotecatl <yotecatlwebprog@gmail.com>
2024-02-03 14:27:08 -0600

    Pagina con bootstrap y js

---
+++ pagina boots y js/main.js
@@ -0,0 +1,13 @@
+
+alert("Bienvenido");
+
+confirm("Entrar");
+
+let nombre = prompt("Cúal es tu nombre?");
+let apellido = prompt("Cual es tu apellido?");
+
+let nombreC = nombre + " " + apellido
+
+console.log(nombre, apellido)
+
+alert(nombreC)
