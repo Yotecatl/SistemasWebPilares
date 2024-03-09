d7194ed01dc9d99f36b0d02eb3dd1a9f3af371b4
Yotecatl <yotecatlwebprog@gmail.com>
2024-03-09 15:09:37 -0600

    Ejemplo calificaciones

---
+++ Objetos_literales/Ejercicios/EjemploCalificaciones/main.js
@@ -0,0 +1,19 @@
+//creamos el objeto
+alumno = {
+    nombre: string,
+    folio: numero,
+    calificaciones: [],  //definir una lista
+    // metodos
+    introCal: function(cal){
+        this.calificaciones.push();
+    },
+
+    promCal: function(){
+        let promedio = 0;
+        for(let i=0; i<calificaciones.leght; i++){
+            let promedio = promedio + this.calificaciones[i];
+        }
+        promedio = promedio/calificaciones.leght
+    }
+
+}
