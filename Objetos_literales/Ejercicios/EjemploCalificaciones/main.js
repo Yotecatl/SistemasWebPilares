c1a6ec9f76b26ccb1db6efbd68452557845cb4ca
Yotecatl <yotecatlwebprog@gmail.com>
2024-03-09 18:20:20 -0600

    Programa Calificaciones

--- Objetos_literales/Ejercicios/EjemploCalificaciones/main.js
+++ Objetos_literales/Ejercicios/EjemploCalificaciones/main.js
@@ -1,19 +1,38 @@
 //creamos el objeto
 alumno = {
-    nombre: string,
-    folio: numero,
+    nombre: "",
+    folio: 0,
     calificaciones: [],  //definir una lista
     // metodos
     introCal: function(cal){
-        this.calificaciones.push();
+        this.calificaciones.push(cal);
+        //console.log(this.calificaciones)
     },
 
-    promCal: function(){
+    promCal: function(calEva){
         let promedio = 0;
-        for(let i=0; i<calificaciones.leght; i++){
-            let promedio = promedio + this.calificaciones[i];
+        for(let i=0; i<alumno.calificaciones.length; i++){
+             promedio = promedio + this.calificaciones[i];
+             //console.log(promedio);
         }
-        promedio = promedio/calificaciones.leght
+        promedio = promedio/alumno.calificaciones.length;
+        if(promedio>=calEva){
+            return this.nombre + " esta aprobado tiene un promedio: " +promedio;
+        }
+        else{
+            return this.nombre + " esta reprobado tiene un promedio: " +promedio;
+        }
     }
 
 }
+alumno.nombre = "Pedro"
+alumno.folio = 23456
+
+console.log("Nombre: " +alumno.nombre);
+console.log("Folio: " +alumno.folio);
+alumno.introCal(2)
+alumno.introCal(3)
+alumno.introCal(5)
+//console.log(alumno.calificaciones);
+console.log("Calificaciones: " +alumno.calificaciones);
+console.log(alumno.promCal(6));
